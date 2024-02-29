8a6398666e5fab0b25916a6ac2616edb6811c23c
MiniAshura <157052292+MiniAshura@users.noreply.github.com>
2024-02-29 22:17:53 +0100

    Add files via upload

--- Snake_game_v1.py
+++ Snake_game_v1.py
@@ -1,46 +1,70 @@
-def game_map(coordinates):
+import random
 
-    grid = [] #emty
-    for _ in range(10):
+
+go_on = True
+eat = True
+
+map_width = 20
+map_height = 20
+
+def game_map(coordinates, food, width, height):
+
+    grid = []
+    for _ in range(width):
         row = []
-        for _ in range(10):
+        for _ in range(height):
             row.append('.')  
         grid.append(row) #dots
+    
 
     
     for X, Y in coordinates:
-        if 0 <= X < 10 and 0 <= Y < 10:  
+        if 0 <= X < height and 0 <= Y < width:  
             grid[X][Y] = 'X' # Replace dots with X 
     
+    for X, Y in food:
+        if 0 <= X < height and 0 <= Y < width:  
+            grid[X][Y] = 'F'
     
     for row in grid:
         print(' '.join(row))
 
 # for testing this function use ex.: game_map([(0,0),(0,1),(0,2)])
         
-def movement(coordinates, direction):
+def movement(coordinates, direction, food, width, height):
     
     last_X, last_Y = coordinates[-1]
     
+    direction_map = {'n': (-1, 0), 's': (1, 0), 'e': (0, 1), 'w': (0, -1)}
+    dx, dy = direction_map[direction]
+    new_coor = (last_X + dx, last_Y + dy)
     
-    if direction == 'n':  # North
-        new_coor = (last_X - 1, last_Y)
-    elif direction == 's':  # South
-        new_coor = (last_X + 1, last_Y)
-    elif direction == 'e':  # East
-        new_coor = (last_X, last_Y + 1)
-    elif direction == 'w':  # West
-        new_coor = (last_X, last_Y - 1)
-    else:
-        print("Invalid input, try again")
-        return
-    coordinates.append(new_coor)
+    if 0 <= new_coor[0] < height and 0 <= new_coor[1] < width:
+        if new_coor in food:
+            food.remove(new_coor)
+            coordinates.append(new_coor)
+            food.append(snake_food(coordinates + food))
+        elif new_coor not in coordinates:
+            coordinates.append(new_coor)
+            coordinates.pop(0)
+        else:
+            return False  # Snake ran into itself
+        return True
+    return False
+
+    
+def snake_food(no_food):
+    while eat:
+        food_point = (random.randint(0, height - 1), random.randint(0, width -1))
+        if food_point not in no_food:
+            return food_point
 
 coordinates = [(0,0),(0,1),(0,2)]
-go_on = True
+food = [snake_food(coordinates, map_width, map_height)]
+
 
 while go_on:
-    game_map(coordinates)  
+    game_map(coordinates, food, map_width, map_height)
     user_direction = input("What is your movement (north, south, east, west)? Type 'end' to stop the game: ").lower()
     
     map= {'north': 'n', 'south': 's', 'east': 'e', 'west': 'w'}
@@ -50,5 +74,9 @@ while go_on:
         print("Game over.")    
     elif user_direction in map:
         movement(coordinates, map[user_direction])
+    elif user_direction not in map:
+        print("Wrong move, try again.")
     else:
         print("Invalid input, try again.")
+
+
